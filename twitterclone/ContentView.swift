fe955ad916b8c8c224e8df4009cbd297df339a5d
irisida <20445648+irisida@users.noreply.github.com>
2021-05-02 15:23:28 +0100

    Initial Commit

---
+++ twitterclone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  twitterclone
+//
+//  Created by ed on 02/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ twitterclone/twittercloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twittercloneApp.swift
+//  twitterclone
+//
+//  Created by ed on 02/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct twittercloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
