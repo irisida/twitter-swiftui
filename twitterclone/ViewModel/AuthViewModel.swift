05aa0feaa183c6084aec881f0feaf388333c66be
irisida <20445648+irisida@users.noreply.github.com>
2021-05-05 16:13:52 +0100

    phase 1 of forebase create user code. Not clean. Need to refactor

--- twitterclone/ViewModel/AuthViewModel.swift
+++ twitterclone/ViewModel/AuthViewModel.swift
@@ -14,18 +14,54 @@ class AuthViewModel: ObservableObject {
         
     }
     
+    // Uses UIImage type for profileImage in order to utlisise the
+    // .jpegData(compressinQuality) method. This is not available
+    // to the standard SwiftUI Image class at this time.
     func signUp(fullname: String, username: String, email: String, password: String, profileImage: UIImage) {
         
         print("DEBUG-> email is   : \(email)")
         print("DEBUG-> password is: \(password)")
         
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let imageFilename = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child(imageFilename)
+        
+        storageRef.putData(imageData, metadata: nil) { _, error in
             if let error = error {
-                print("ERROR -> error: \(error.localizedDescription)")
+                print("ERROR -> Failed to upload image error: \(error.localizedDescription)")
                 return
             }
             
-            print("DEBUG -> successfully signed up a new User.")
+            print("DEBUG -> successfully uploded new user photo.")
+            
+            storageRef.downloadURL { url, _ in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("ERROR -> error: \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let user = result?.user else { return }
+                    
+                    let data = ["email": email,
+                                "username": username,
+                                "fullname": fullname,
+                                "profileImageUrl": profileImageUrl,
+                                "uid": user.uid ]
+                    
+                    Firestore.firestore().collection("users").document(user.uid).setData(data) { _ in
+                        print("DEBUG -> successfully signed up a new User.")
+                    }
+                    
+                    
+                }
+            }
+            
+            
         }
+        
+        
     }
 }
